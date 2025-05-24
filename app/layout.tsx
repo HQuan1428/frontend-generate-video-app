b5dd0dea82d5e98b595190f048dcb94bd76fae10
Quân Nguyễn Hải <quannguyenhai51@gmail.com>
2025-05-24 15:32:57 +0700

    Setup project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,41 @@
+import type { Metadata } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+
+import './globals.css'
+import Providers from '../components/dashboard/Providers'
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
+
+export const metadata: Metadata = {
+  title: 'AI Shorts Generator',
+  description: 'Easily create short videos with subtitles and audio using AI',
+}
+
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en' suppressHydrationWarning={true}>
+      <head>
+        <link rel="stylesheet" href="/favicon.ico" />
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Providers>
+          <main className='mx-auto'>{children}</main>
+        </Providers>
+      </body>
     </html>
-  );
+  )
 }
